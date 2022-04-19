4944355e1e646e5fe646d4c525ab82d68498b2eb
amalina30 <96506811+amalina30@users.noreply.github.com>
2022-04-19 12:34:42 +0700

    Add files via upload

---
+++ Karyawan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4334
+{
+    class Karyawan
+    {
+        //Properties
+        public int Nik { get; set; }
+        public string Nama { get; set; }
+        public int GajiBulanan { get; set; }
+        public int GajiNaik { get; set; }
+
+        //Constructor
+        public Karyawan(int Nik, string Nama, int GajiBulanan)
+        {
+            this.Nik = Nik;
+            this.Nama = Nama;
+
+            if (GajiBulanan < 0)
+            {
+                this.GajiBulanan = 0;
+                GajiNaik = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+            else
+            {
+                this.GajiBulanan = GajiBulanan;
+                GajiNaik = Convert.ToInt32((GajiBulanan * 1.1));
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4334
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Membuat Objek
+            Karyawan satu = new Karyawan(190302123, "Paijo", 3000000);
+            Karyawan dua = new Karyawan(190302124, "Jono", 2000000);
+
+            Console.WriteLine("No | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("================================================");
+            Console.WriteLine("1. | {0} {1} \t\t | {2}\t", satu.Nik, satu.Nama, satu.GajiBulanan);
+            Console.WriteLine("1. | {0} {1} \t\t | {2}\t", dua.Nik, dua.Nama, dua.GajiBulanan);
+
+            Console.WriteLine("KENAIKAN GAJI 10%");
+
+            Console.WriteLine("No | NIK / NAMA\t\t | Gaji Bulanan\t |");
+            Console.WriteLine("================================================");
+            Console.WriteLine("1. | {0} {1} \t\t | {2}\t", satu.Nik, satu.Nama, satu.GajiNaik);
+            Console.WriteLine("1. | {0} {1} \t\t | {2}\t", dua.Nik, dua.Nama, dua.GajiNaik);
+
+            Console.ReadKey();
+        }
+    }
+}
